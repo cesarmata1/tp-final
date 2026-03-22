0c060ab3d8cabed3069e746bf28db4d1a9ac1db0
Cesar Mata rodriguez <ccesarmata@gmail.com>
2026-03-22 19:48:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.end("EXAMEN OK NODE 🚀");
 });
 
